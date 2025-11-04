b0a2cd319bb6bf7fe177b2e608cbb355780fb34d
camilla gomes <camilla.gomes@sp.senai.br>
2025-11-04 15:48:55 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+
+
+async function Get (cep) {
+
+    document.getElementById(cep)
+    const Bruto = `https//viacep.com.br/ws/${cep}/json`
+    Bruto = cep
+    console.log(Bruto)
+    try{
+   
+    }
+    catch(error) {
+    console.log(error)
+ }
+}
